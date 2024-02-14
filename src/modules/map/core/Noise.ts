5a0524474b5a311c2db1e5239505e65d73219517
Titouan Lacombe--Fabre <titouan.lacombe---fabre@etu.univ-cotedazur.fr>
2024-02-14 18:35:37 +0100

    feat: Options param for MapPresenter constructor #4

--- src/modules/map/core/Noise.ts
+++ src/modules/map/core/Noise.ts
@@ -1,3 +1,4 @@
+//@ts-nocheck
 import * as tumult from 'tumult';
 
 export class Noise {
--- src/modules/map/presenter/MapPresenter.ts
+++ src/modules/map/presenter/MapPresenter.ts
@@ -2,12 +2,24 @@ import { MapModel } from '../model/MapModel.ts';
 import { MapView } from '../view/Babylon/MapView.ts';
 import { Scene } from '@babylonjs/core';
 
+
+type MapPresenterOptions = {
+  size?: number;
+  seed?: number | string;
+}
+
+const DEFAULT_OPTIONS: Readonly<MapPresenterOptions> = Object.freeze({
+  size: 100,
+  seed: Math.random(),
+});
 export class MapPresenter {
   private _mapModel: MapModel;
   private _view: MapView;
 
-  constructor(scene: Scene) {
-    this._mapModel = new MapModel(100);
+  constructor(scene: Scene, options: MapPresenterOptions = {}) {
+    const finalOptions = { ...DEFAULT_OPTIONS, ...options };
+    this._mapModel = new MapModel(finalOptions.size!, finalOptions.seed);
     this._view = new MapView(scene, this._mapModel);
   }
+
 }
