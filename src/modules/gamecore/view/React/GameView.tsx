0d6107d61fc4da5d547d704b9d6f65da7086ece8
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-02-22 17:15:07 +0100

    fix: fix babylon init #6

--- src/modules/gamecore/view/React/GameView.tsx
+++ src/modules/gamecore/view/React/GameView.tsx
@@ -1,17 +1,15 @@
-import { View } from '../Babylon/View.ts';
 import BabylonScene from '../../../../component/BabylonScene.tsx';
 import { RoundStatusBar } from './RoundStatusBar.tsx';
 import { MainView } from './MainView.tsx';
 import React from 'react';
+import { Scene } from '@babylonjs/core';
 
 interface GameViewProps {
   mainView: MainView;
+  babylon: { onSceneReady: (scene: Scene) => void; onRender: (scene: Scene) => void };
 }
 
-export const GameView: React.FC<GameViewProps> = ({ mainView }) => {
-  const view = new View();
-  const babylon = view.init();
-
+export const GameView: React.FC<GameViewProps> = ({ mainView, babylon }) => {
   return (
     <div>
       <BabylonScene antialias={true} onSceneReady={babylon.onSceneReady} onRender={babylon.onRender} id="my-canvas" />
