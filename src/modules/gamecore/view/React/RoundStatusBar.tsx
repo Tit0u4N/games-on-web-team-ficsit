64d8fe1dc152cd507b8e4847d0e60a303ef14d43
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-02-26 15:14:57 +0100

    fix: month and year counter #6

--- src/modules/gamecore/view/React/RoundStatusBar.tsx
+++ src/modules/gamecore/view/React/RoundStatusBar.tsx
@@ -51,8 +51,8 @@ export const RoundStatusBar: React.FC<RoundStatusBarProps> = ({ nextRound, round
         </NavbarItem>
       </NavbarContent>
       <NavbarContent>
-        <div>Year {Math.floor(round / 12)}</div>
-        <div>Month {round % 12}</div>
+        <div>Year {Math.floor(round / 12) + 1}</div>
+        <div>Month {(round % 12) + 1}</div>
         <div className={'season-status'}>
           <span className={`season-bar ${getSeason(round - 1)}`}>{getSeasonIcon(round - 1)}</span>
           <span className={`season-bar ${getSeason(round)} current`}>{getSeasonIcon(round)}</span>
