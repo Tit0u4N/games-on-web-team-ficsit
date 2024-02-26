00fb7ad2c6449024fb5a51bf8cedcb4307634365
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-02-26 15:01:51 +0100

    feat: add round season bar #6

--- src/modules/gamecore/view/React/RoundStatusBar.tsx
+++ src/modules/gamecore/view/React/RoundStatusBar.tsx
@@ -1,16 +1,35 @@
 import { Button, Navbar, NavbarContent, NavbarItem } from '@nextui-org/react';
 import React from 'react';
+import './RoundStatusBar.scss';
 
 interface RoundStatusBarProps {
   nextRound: () => void;
   round: number;
 }
 
+function getSeason(round: number) {
+  if (round < 0) round = 12 + round;
+  round = round % 12;
+  if (round >= 0 && round < 3) return 'summer';
+  if (round >= 3 && round < 6) return 'autumn';
+  if (round >= 6 && round < 9) return 'winter';
+  if (round >= 9 && round < 12) return 'spring';
+}
+
+function getSeasonIcon(round: number) {
+  if (round < 0) round = 12 + round;
+  round = round % 12;
+  if (round >= 0 && round < 3) return '☀';
+  if (round >= 3 && round < 6) return '🍂';
+  if (round >= 6 && round < 9) return '❄';
+  if (round >= 9 && round < 12) return '🌸';
+}
+
 export const RoundStatusBar: React.FC<RoundStatusBarProps> = ({ nextRound, round }) => {
   const [isEventsMenuOpen, setIsEventsMenuOpen] = React.useState(false);
   const [isInventoryMenuOpen, setIsInventoryMenuOpen] = React.useState(false);
   return (
-    <Navbar className={'fixed-top'}>
+    <Navbar className={'fixed-top navbar'}>
       <NavbarContent>
         <NavbarItem>
           <Button
@@ -33,6 +52,12 @@ export const RoundStatusBar: React.FC<RoundStatusBarProps> = ({ nextRound, round
       </NavbarContent>
       <NavbarContent>
         <div>Round {round}</div>
+        <div className={'season-status'}>
+          <span className={`season-bar ${getSeason(round - 1)}`}>{getSeasonIcon(round - 1)}</span>
+          <span className={`season-bar ${getSeason(round)} current`}>{getSeasonIcon(round)}</span>
+          <span className={`season-bar ${getSeason(round + 1)}`}>{getSeasonIcon(round + 1)}</span>
+          <span className={`season-bar ${getSeason(round + 2)}`}>{getSeasonIcon(round + 2)}</span>
+        </div>
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem>
