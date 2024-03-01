0a4ee5408d0c18c6fcbbc5dbfa52aefd7d5d8115
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-03-01 18:34:27 +0100

    fix: a test. #2

--- src/modules/character/test/model.spec.ts
+++ src/modules/character/test/model.spec.ts
@@ -5,7 +5,7 @@ describe('Building model', () => {
   let model: Character;
 
   beforeEach(() => {
-    model = new Character(0, '0', '0', 0, new Attributes(0, 0, false));
+    model = new Character(0, 'no_manes', 'NULL', 0, new Attributes(0, 0, false), '/no_image.png');
   });
 
   it('should be defined', () => {
