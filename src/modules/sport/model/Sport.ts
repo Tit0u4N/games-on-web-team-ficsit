e697ab695b8f84190f52b9021a9585fbfe939986
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-03-16 12:03:02 +0100

    feat: adding the constructor. #44

--- src/modules/sport/model/Sport.ts
+++ src/modules/sport/model/Sport.ts
@@ -1,4 +1,18 @@
-export class Sport {}
+export class Sport {
+  private sport: SportType;
+
+  constructor(sport: SportType) {
+    this.sport = sport;
+  }
+
+  public getSport(): SportType {
+    return this.sport;
+  }
+
+  public setSport(sport: SportType): void {
+    this.sport = sport;
+  }
+}
 
 export enum SportType {
   ATHLETISM = 'Athletism',
