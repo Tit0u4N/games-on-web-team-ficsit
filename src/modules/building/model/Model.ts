c89687b17f5b1ce9a6b752158a9645b932b8ab54
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-03-26 21:55:49 +0100

    feat: adding the rotation sport. #43

--- src/modules/building/model/Model.ts
+++ src/modules/building/model/Model.ts
@@ -2,21 +2,36 @@ import { SportType } from '../../sport/model/Sport.ts';
 import { Vector3 } from '@babylonjs/core';
 
 export class Building {
-  private _sportType: SportType;
+  private static DEFAULT_ROTATION: number = 5;
+  private _sportType: SportType[];
+  private _actualSport: SportType;
+  private rotation: number;
   private _position: Vector3;
   private _roundWaiting: number;
 
-  constructor(sportType: SportType, position: Vector3) {
+  constructor(sportType: SportType[], position: Vector3) {
     this._sportType = sportType;
     this._position = position;
     this._roundWaiting = 0;
+    this.rotation = Building.DEFAULT_ROTATION;
+    this._actualSport = this._sportType[0];
   }
 
-  get sportType(): SportType {
+  public update() {
+    if (this.rotation > 0) {
+      this.rotation--;
+    } else {
+      this.rotation = Building.DEFAULT_ROTATION;
+      const index = this._sportType.indexOf(this._actualSport);
+      this._actualSport = this._sportType[(index + 1) % this._sportType.length];
+    }
+  }
+
+  get sportType(): SportType[] {
     return this._sportType;
   }
 
-  set sportType(sportType: SportType) {
+  set sportType(sportType: SportType[]) {
     this._sportType = sportType;
   }
 
