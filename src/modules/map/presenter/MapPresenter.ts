816b8a906f05313ffb16068f30dce3ae0e6b1631
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-04-02 11:18:54 +0200

    feat: setup GamePlacer #52

--- src/modules/map/presenter/MapPresenter.ts
+++ src/modules/map/presenter/MapPresenter.ts
@@ -1,9 +1,9 @@
 import { IMapModelPresenter, MapModel } from '../model/MapModel.ts';
 import { MapView } from '../view/Babylon/MapView.ts';
-import { Scene, Vector3 } from '@babylonjs/core';
+import { Color3, PBRMaterial, Scene, Vector3 } from '@babylonjs/core';
 import { IGraphTiles } from '../model/GraphTilesModel.ts';
 import { importModel } from '../../../core/ModelImporter.ts';
-import { getPosition } from '../core/GamePlacer.ts';
+import { getPosition, getTilePosition, PositionTypes } from '../core/GamePlacer.ts';
 
 type MapPresenterOptions = {
   size?: number;
@@ -26,20 +26,41 @@ export class MapPresenter {
     this._view = new MapView(this._mapModel);
   }
 
-  async init(scene: Scene) {
+  init(scene: Scene) {
     this._mapModel.init();
     this._view.init(scene);
-    const mesh = await importModel('', '', 'scene.gltf', scene);
-    mesh.position = getPosition(this._view.getTile(12, 25) as never);
-    mesh.scaling = new Vector3(0.2, 0.2, 0.2);
-    mesh.rotation = new Vector3(0, -Math.PI / 2, 0);
-    const mesh2 = await importModel('', '', 'trees.gltf', scene);
-    mesh2.position = getPosition(this._view.getTile(12, 24) as never);
-    mesh2.scaling = new Vector3(1, 1, 1);
-    mesh2.rotation = new Vector3(0, 0, 0);
+    this.testObjects(scene).then();
   }
 
   public getDisplacementGraph(): IGraphTiles {
     return this._mapModel.displacementGraph;
+  }
+
+  private async testObjects(scene: Scene) {
+    const mesh = await importModel('', '', 'scene.gltf', scene);
+    mesh.position = getTilePosition(getPosition(this._view.getTile(12, 25) as never, PositionTypes.PAWN), 2, 0);
+    mesh.scaling = new Vector3(0.2, 0.2, 0.2);
+    mesh.rotation = new Vector3(0, -Math.PI / 2, 0);
+    (mesh.material as PBRMaterial).albedoColor = Color3.FromHexString('#ff0000');
+    const meshCopy = await importModel('', '', 'scene.gltf', scene);
+    meshCopy.position = getTilePosition(getPosition(this._view.getTile(12, 25) as never, PositionTypes.PAWN), 2, 1);
+    meshCopy.scaling = new Vector3(0.2, 0.2, 0.2);
+    meshCopy.rotation = new Vector3(0, -Math.PI / 2, 0);
+    (meshCopy.material as PBRMaterial).albedoColor = Color3.FromHexString('#00ff00');
+    const meshCopy2 = await importModel('', '', 'scene.gltf', scene);
+    meshCopy2.position = getTilePosition(getPosition(this._view.getTile(12, 26) as never, PositionTypes.PAWN), 1, 0);
+    meshCopy2.scaling = new Vector3(0.2, 0.2, 0.2);
+    meshCopy2.rotation = new Vector3(0, -Math.PI / 2, 0);
+    (meshCopy2.material as PBRMaterial).albedoColor = Color3.FromHexString('#0000ff');
+    const mesh3 = await importModel('', '', 'trees2.gltf', scene, true);
+    mesh3.position = getPosition(this._view.getTile(12, 24) as never, PositionTypes.DECORATION).add(
+      new Vector3(0, -0.3, 0),
+    );
+    mesh3.scaling = new Vector3(1.2, 1.2, 1.2);
+    mesh3.rotation = new Vector3(0, 0, 0);
+    const mesh2 = await importModel('', '', 'trees.gltf', scene, true);
+    mesh2.position = getPosition(this._view.getTile(12, 25) as never, PositionTypes.BUILDING);
+    mesh2.scaling = new Vector3(0.7, 0.7, 0.7);
+    mesh2.rotation = new Vector3(0, Math.PI / 4, 0);
   }
 }
