df15af7359b8b6065aaa26bce4335b4956c8e58e
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-04-08 18:40:25 +0200

    fix: unit tests #45

--- src/modules/dice/test/presenter.spec.ts
+++ src/modules/dice/test/presenter.spec.ts
@@ -1,7 +1,23 @@
 import { DicePresenter } from '../presenter/DicePresenter.ts';
+//Mock Dice3D
+jest.mock('../view/babylon/Dice3D.ts', () => {
+  return {
+    Dice3D: jest.fn().mockImplementation(() => {
+      return {
+        delete: jest.fn(),
+      };
+    }),
+  };
+});
 
 describe('Building model', () => {
   let presenter: DicePresenter;
+
+  beforeEach(() => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    presenter = new DicePresenter(null);
+  });
 
   it('should be defined', () => {
     expect(presenter).toBeDefined();
