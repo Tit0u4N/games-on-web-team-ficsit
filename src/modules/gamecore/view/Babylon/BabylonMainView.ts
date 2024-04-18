9043ffdb5f4116974961fab162a69de3229bd3a9
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-04-18 10:50:10 +0200

    feat: adding the ArcRotateCamera and make the camera moving like in rts game. #53

--- src/modules/gamecore/view/Babylon/BabylonMainView.ts
+++ src/modules/gamecore/view/Babylon/BabylonMainView.ts
@@ -1,15 +1,14 @@
 import {
+  ArcRotateCamera,
   Camera,
   Engine,
   EngineOptions,
   HemisphericLight,
   Scene,
   SceneOptions,
-  Tools,
-  UniversalCamera,
   Vector3,
 } from '@babylonjs/core';
-import { FreeCameraKeyboardInputs } from './FreeCameraKeyboardInputs.ts';
+import { ArcRotateCameraKeyboardInputs } from './ArcRotateCameraKeyboardInputs.ts';
 
 type BabylonMainViewOptions = {
   antialias: boolean;
@@ -25,7 +24,7 @@ const DEFAULT_OPTIONS: Readonly<BabylonMainViewOptions> = {
   sceneOptions: {},
 };
 
-class BabylonMainView {
+export class BabylonMainView {
   private _options: BabylonMainViewOptions;
   private _canvas!: HTMLCanvasElement;
   private _engine!: Engine;
@@ -48,73 +47,34 @@ class BabylonMainView {
   }
 
   onSceneReady(): void {
-    const canvas = this._scene.getEngine().getRenderingCanvas();
-
     // This creates and positions a free camera (non-mesh)
-    const camera = new UniversalCamera('camera1', new Vector3(0, 100, -50), this.scene);
+    const camera = new ArcRotateCamera('camera', 0, 0, 10, new Vector3(90, 150, -50), this.scene);
 
-    // This targets the camera to scene origin
-    camera.setTarget(Vector3.Zero());
+    // The target should also be adjusted so that the camera is looking at the correct location
+    camera.setTarget(new Vector3(90, 0, 50));
 
     // This attaches the camera to the canvas
     camera.mode = Camera.PERSPECTIVE_CAMERA;
-    camera.speed = 0.1;
-    camera.fov = 1.0;
+    camera.speed = 5;
+    camera.fov = 0.5;
 
-    camera.metadata = {
-      // mouse & keyboard properties
-      // Set by camera inputs. Defines, which input moves the camera (mouse or keys)
-      movedBy: null,
-      // target position, the camera should be moved to
-      targetPosition: camera.position.clone(),
-      // radius, that is used to rotate camera
-      // initial value dependent from camera position and camera target
-      radius: new Vector3(camera.position.x, 0, camera.position.z)
-        .subtract(new Vector3(camera.target.x, 0, camera.target.z))
-        .length(),
-      // helper variable, to rotate camera
-      rotation: Tools.ToRadians(180) + camera.rotation.y,
-      // speed for rotation
-      rotationSpeed: 0.02,
-      // boundaries for x and z
-      minX: -30,
-      maxX: 30,
-      minZ: -30,
-      maxZ: 30,
+    const canvas = this.scene.getEngine().getRenderingCanvas();
 
-      // mousewheel properties
-      // similar to targetPosition, targetZoom contains the target value for the zoom
-      targetZoom: camera.fov,
-      // zoom boundaries
-      maxZoom: 1.4,
-      minZoom: 0.5,
-      // speed for zoom
-      zoom: 0.005,
-      // zoom distance per mouse wheel interaction
-      zoomSteps: 0.2,
-    };
-
-    camera.inputs.clear();
+    // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
-    camera.inputs.add(new FreeCameraKeyboardInputs(camera));
+    camera.inputs.clear();
+    const arcRotateCameraKeyboardInputs = new ArcRotateCameraKeyboardInputs(camera);
+    camera.inputs.add(arcRotateCameraKeyboardInputs);
+    arcRotateCameraKeyboardInputs.attachControl(true);
 
     // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    const light = new HemisphericLight('light', new Vector3(0, 1, 0), this._scene);
+    const light = new HemisphericLight('light', new Vector3(0, 1, 0), this.scene);
 
     // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
+    light.intensity = 0.5;
   }
 
-  onRender(): void {
-    this._engine.runRenderLoop(() => {
-      if (this._scene) {
-        this._scene.render();
-      }
-    });
-    window.addEventListener('resize', () => {
-      this._engine.resize();
-    });
-  }
+  onRender(): void {}
 
   get engine(): Engine {
     return this._engine;
@@ -136,5 +96,3 @@ class BabylonMainView {
     this._canvas = value;
   }
 }
-
-export { BabylonMainView };
