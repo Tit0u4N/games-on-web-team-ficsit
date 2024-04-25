10ad940ad183e26072d44ad7fd4918f6c561bc8e
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-04-25 23:02:47 +0200

    feat: refactor statistics. #62

--- src/modules/character/model/Statistics.ts
+++ src/modules/character/model/Statistics.ts
@@ -1,25 +1,55 @@
-import { Sport } from '../../sport/model/Sport';
+import { Sport, SportType } from '../../sport/model/Sport';
+import { config } from '../../../core/Interfaces.ts';
 
 export class Statistics {
-  private _items: Sport[] = [];
+  private readonly _stats: Map<Sport, number>;
 
-  public addItem(item: Sport): void {
-    this._items.push(item);
+  public constructor(stats: Map<Sport, number> = new Map<Sport, number>()) {
+    console.log(Object.values(SportType));
+    this._stats = config.statistics.setDefaultStats ? Statistics.createRandomStats() : stats;
   }
 
-  public removeItem(item: Sport): void {
-    const index = this._items.indexOf(item);
-    if (index !== -1) {
-      this._items.splice(index, 1);
+  public static createFromJsObject(jsObject: object): Statistics {
+    const stats = new Map<Sport, number>();
+    for (const sport of Object.values(SportType)) {
+      stats.set(new Sport(sport), jsObject[sport]);
     }
+    return new Statistics(stats);
   }
 
-  public get items(): Sport[] {
-    return this._items;
+  public static createRandomStats(): Map<Sport, number> {
+    const stats = new Map<Sport, number>();
+    // Share not equitably the statsToShare between all sports and downgrades the statsToShare
+    for (const sportType of Object.values(SportType)) {
+      // the stats can be between 2 and 13
+      stats.set(new Sport(sportType), Math.floor(Math.random() * 12) + 2);
+    }
+    console.log(stats);
+    return stats;
   }
 
-  public toString(): string {
-    // TODO: Return a table with the statistics
-    return '';
+  get stats(): Map<Sport, number> {
+    return this._stats;
+  }
+
+  public getStat(sport: Sport): number {
+    return this._stats.get(sport) || 0;
+  }
+
+  public setStat(sport: Sport, value: number): void {
+    if (value < 0) {
+      value = 0;
+    } else if (value > 20) {
+      value = 20;
+    }
+    this._stats.set(sport, value);
+  }
+
+  public addStat(statistics: Statistics): Statistics {
+    const newStats: Statistics = new Statistics();
+    for (const [sport, value] of this._stats) {
+      newStats.setStat(sport, value + statistics.getStat(sport));
+    }
+    return newStats;
   }
 }
--- src/modules/object/model/UsableObject.ts
+++ src/modules/object/model/UsableObject.ts
@@ -1,17 +1,1 @@
-export class UsableObject {
-  private readonly _name: string;
-  private readonly _image: string;
-
-  constructor(name: string, image: string) {
-    this._name = name;
-    this._image = image;
-  }
-
-  public get name(): string {
-    return this._name;
-  }
-
-  public get image(): string {
-    return this._image;
-  }
-}
+export class UsableObject {}
