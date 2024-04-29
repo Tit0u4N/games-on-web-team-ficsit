f5b55a9b0bc8fa5c7214359f194f8e92a336723c
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-04-29 21:54:32 +0200

    feat: adding the getEffect function for the object. #62

--- src/modules/object/model/UsableObject.ts
+++ src/modules/object/model/UsableObject.ts
@@ -1,48 +1,132 @@
 import { Statistics } from '../../character/model/Statistics.ts';
+import { Character } from '../../character/model/Character.ts';
+import { Season } from '../../../core/singleton/Season.ts';
 import gameObjectsData from './gameObjects.json';
+import gearsCombinaisons from './completeGearKeys.json';
+import { Sport } from '../../../core/singleton/Sport.ts';
+import { config } from '../../../core/Interfaces.ts';
 
 export class UsableObject {
+  private readonly _id: number;
   private readonly _name: string;
   private readonly _image: string;
   private readonly _statsIncrease: Statistics;
+  private readonly _conditions: Condition[];
 
-  constructor(name: string, image: string, statsIncrease: Statistics) {
+  constructor(id: number, name: string, image: string, statsIncrease: Statistics, conditions: Condition[] = []) {
+    this._id = id;
     this._name = name;
-    this._image = 'objectImages/' + image;
+    this._image = config.usableObject.basePath + image;
     this._statsIncrease = statsIncrease;
+    this._conditions = conditions;
   }
 
   get statsIncrease(): Statistics {
     return this._statsIncrease;
   }
 
-  get name(): string {
-    return this._name;
+  getEffect(character: Character, season: Season) {
+    // Copy the current stats
+    const modifiedStats: Statistics = new Statistics(this._statsIncrease);
+
+    // Get complete gear bonus
+    const completeGearBonus = this.getCompleteGearBonus(character, gears);
+    if (completeGearBonus) {
+      modifiedStats.addStat(completeGearBonus);
+    }
+
+    for (const condition of this._conditions) {
+      if (this.conditionMatches(condition, character, season)) {
+        for (const statBonus of condition.statsIncrease) {
+          const sport = Sport.getByName(statBonus.sport);
+          if (sport) {
+            modifiedStats.set(sport, modifiedStats.get(sport) + statBonus.bonus);
+          } else {
+            console.error(`Sport not found: ${statBonus.sport}`);
+          }
+        }
+      }
+
+      return modifiedStats;
+    }
   }
 
-  get image(): string {
-    return this._image;
+  private getCompleteGearBonus(character: Character, gears: IGears[]): Statistics | null {
+    for (const gear of gears) {
+      if (gear.allEquipments.every((id) => character.inventory.equippedItemsIds.includes(id))) {
+        const bonus = Statistics.createFromJsObject(gear.statsIncrease);
+        return bonus;
+      }
+    }
+    return null;
   }
-}
 
-interface GameObjectData {
-  name: string;
-  sport: string;
-  image: string;
-  statsIncrease: StatIncrease[];
-}
+  private conditionMatches(condition: Condition, character: Character, season: Season) {
+    switch (condition.type) {
+      case 'terrain':
+        return character.tile?.subBiome === condition.terrain && season.name === condition.season;
+      // Here, we can add more condition types
+      default:
+        return false;
+    }
+  }
 
-interface StatIncrease {
-  sport: string;
-  bonus: number;
+  get id(): number {
+    return this._id;
+  }
 }
 
 function parseGameObjects(data: GameObjectData[]): UsableObject[] {
   return data.map((obj) => {
+    const conditions = obj.conditions ? obj.conditions.map(parseCondition) : [];
     const statsIncrease = Statistics.createFromJsObject(obj.statsIncrease);
-    return new UsableObject(obj.name, obj.image, statsIncrease);
+    return new UsableObject(obj.id, obj.name, obj.image, statsIncrease, conditions);
   });
+}
+
+function parseCondition(conditionData: Condition): Condition {
+  switch (conditionData.type) {
+    case ConditionType.Terrain:
+      return conditionData;
+    // Here, we can add more condition types
+    default:
+      throw new Error(`Unknown condition type: ${conditionData.type}`);
+  }
 }
 
 // todo read fix lint error
 export const gameObjects: UsableObject[] = parseGameObjects(gameObjectsData);
+export const gears: IGears[] = gearsCombinaisons;
+
+enum ConditionType {
+  Terrain = 'terrain',
+  // Here, we can add more condition types
+}
+
+interface GameObjectData {
+  id: number;
+  name: string;
+  sport: string;
+  image: string;
+  statsIncrease: IStatIncrease[];
+  conditions?: Condition[];
+}
+
+export interface IStatIncrease {
+  sport: string;
+  bonus: number;
+}
+
+interface Condition {
+  type: string;
+  season?: string;
+  terrain?: string;
+  // Here, we can add more condition properties
+  statsIncrease: IStatIncrease[];
+}
+
+interface IGears {
+  gearId: string;
+  allEquipments: number[];
+  statsIncrease: IStatIncrease[];
+}
