1e62d57293de8649f52e3c50c9f5a56f480c3178
Titouan Lacombe--Fabre <titouan.lacombe--fabre@etu.univ-cotedazur.fr>
2024-05-07 12:36:46 +0200

    fix: bracket #60

--- src/modules/tournament/view/React/bracket/Bracket.tsx
+++ src/modules/tournament/view/React/bracket/Bracket.tsx
@@ -1,40 +1,47 @@
 import React from 'react';
-import "./Bracket.scss";
+import './Bracket.scss';
 
 type Props = {
   bracket: BracketObject;
   isChildren?: boolean;
 };
 
-
 export const Bracket: React.FC<Props> = ({ bracket, isChildren = false }) => {
-
   const hasChildren = !!(bracket.children1 || bracket.children2);
 
-  if (!isChildren) {
+  const renderNoChildren = () => {
+    return <p> {bracket.name} </p>;
+  };
+
+  const renderHasChildren = () => {
     return (
-    <div className={"wrapper"}>
-      <div className={"item"}>
-        <div className={"item-parent"}>
-          <p> { bracket.name } </p>
+      <div className={'item'}>
+        <div className={'item-parent'}>
+          <p> {bracket.name} </p>
         </div>
-        <div className={"item-children"}>
-          {
-              <div className={"item-child"}>
-                { bracket.children1 && <Bracket bracket={bracket.children1} /> }
-                { bracket.children2 && <Bracket bracket={bracket.children2} /> }
-              </div>
-          }
-          { bracket.children1 && <Bracket bracket={bracket.children1} /> }
-          { bracket.children2 && <Bracket bracket={bracket.children2} /> }
+        <div className={'item-children'}>
+          {bracket.children1 && (
+            <div className={'item-child'}>
+              <Bracket bracket={bracket.children1} isChildren={true} />{' '}
+            </div>
+          )}
+          {bracket.children2 && (
+            <div className={'item-child'}>
+              <Bracket bracket={bracket.children2} isChildren={true} />{' '}
+            </div>
+          )}
         </div>
       </div>
-    </div>
     );
+  };
+  if (isChildren) {
+    if (hasChildren) {
+      return renderHasChildren();
+    } else {
+      return renderNoChildren();
+    }
   }
-  return (
-
-  );
+  return <div className={'wrapper'}>{renderHasChildren()}</div>;
 };
 
 export class BracketObject {
@@ -43,20 +50,19 @@ export class BracketObject {
   private _children2?: BracketObject;
 
   constructor(name: string, children1?: BracketObject, children2?: BracketObject) {
-    this._name = "";
+    this._name = name;
     this._children1 = children1;
     this._children2 = children2;
   }
 
   static testBracket(): BracketObject {
-    const parent3_1 = new BracketObject("parent3_1", new BracketObject("child3_1_1"), new BracketObject("child3_1_2"));
-    const parent3_2 = new BracketObject("parent3_2", new BracketObject("child3_2_1"), new BracketObject("child3_2_2"));
-    const parent3_3 = new BracketObject("parent2_1", parent3_1, parent3_2);
-    const parent3_4 = new BracketObject("parent2_2", new BracketObject("child2_2_1"), new BracketObject("child2_2_2"));
-    const parent2_1 = new BracketObject("parent1", parent3_1, parent3_2);
-    const parent2_2 = new BracketObject("parent2", parent3_3, parent3_4);
-    return new BracketObject("parent", parent2_1, parent2_2);
-
+    const parent3_1 = new BracketObject('parent3_1', new BracketObject('child3_1_1'), new BracketObject('child3_1_2'));
+    const parent3_2 = new BracketObject('parent3_2', new BracketObject('child3_2_1'), new BracketObject('child3_2_2'));
+    const parent3_3 = new BracketObject('parent2_1', parent3_1, parent3_2);
+    const parent3_4 = new BracketObject('parent2_2', new BracketObject('child2_2_1'), new BracketObject('child2_2_2'));
+    const parent2_1 = new BracketObject('parent1', parent3_1, parent3_2);
+    const parent2_2 = new BracketObject('parent2', parent3_3, parent3_4);
+    return new BracketObject('parent', parent2_1, parent2_2);
   }
 
   hasChildren(): boolean {
@@ -71,9 +77,7 @@ export class BracketObject {
     return this._children1;
   }
 
-  get children2(): BracketObject | undefined{
+  get children2(): BracketObject | undefined {
     return this._children2;
   }
-
-
 }
