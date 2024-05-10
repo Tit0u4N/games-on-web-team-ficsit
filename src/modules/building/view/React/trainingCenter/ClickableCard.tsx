84e639853bf9b0aa54ddfd8a4be7182e6e7f113b
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-05-10 11:55:53 +0200

    feat: adding the addition score as a preview on the character card. #80

--- src/modules/building/view/React/trainingCenter/ClickableCard.tsx
+++ src/modules/building/view/React/trainingCenter/ClickableCard.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Card, CardBody } from '@nextui-org/react';
 import { Character } from '../../../../character/model/Character.ts';
 import { CharacterStats } from './CharacterStats.tsx';
+import { TrainingChoice } from './TrainingChoiceCards.tsx';
+import { TrainingCenterModel } from '../../../model/TrainingCenterModel.ts';
 
 interface ClickableCardProps {
   character: Character;
+  trainingCenter: TrainingCenterModel;
+  choice: TrainingChoice | null;
   isSelected: boolean;
   onSelect: () => void;
 }
 
-export const ClickableCard: React.FC<ClickableCardProps> = ({ character, isSelected, onSelect }) => {
+export const ClickableCard: React.FC<ClickableCardProps> = ({ character, trainingCenter, choice, isSelected, onSelect }) => {
   const cardClassName = `flex flex-col gap-1 w-[600px] p-2 mt-4 mb-4 ${
     isSelected ? 'opacity-100' : 'opacity-50'
   }`;
@@ -18,7 +22,7 @@ export const ClickableCard: React.FC<ClickableCardProps> = ({ character, isSelec
     <Card key={character.id} radius={'lg'} className={cardClassName}>
       <CardBody>
         <div onClick={onSelect} className="w-full h-full">
-          <CharacterStats character={character} />
+          <CharacterStats character={character} trainingCenter={trainingCenter} choice={choice} />
         </div>
       </CardBody>
     </Card>
