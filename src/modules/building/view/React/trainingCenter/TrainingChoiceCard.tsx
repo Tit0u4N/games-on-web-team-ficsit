64ac97603a1a7f0bb312dd100323b50243382528
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2024-05-10 17:56:46 +0200

    feat: updating the size of the cards. #80

--- src/modules/building/view/React/trainingCenter/TrainingChoiceCard.tsx
+++ src/modules/building/view/React/trainingCenter/TrainingChoiceCard.tsx
@@ -13,13 +13,13 @@ export const TrainingChoiceCard: React.FC<TrainingChoiceCardProps> = ({ choice, 
   const cardClassName = `w-full h-full ${isSelected ? 'opacity-100' : 'opacity-50'}`;
   return (
     <Card className={cardClassName}>
-      <CardHeader className={'flex flex-col'}>
-        <div>
+      <CardHeader className={'flex flex-col h-[50%]'}>
+        <div className={'mb-[20px]'}>
           <img src={choice.image} alt={choice.label} className="w-full h-32 object-cover" />
         </div>
         <div className="text-center text-xl font-semibold mt-2">{choice.label}</div>
       </CardHeader>
-      <CardBody>
+      <CardBody className={'mt-[20px]'}>
         <div className="flex items-center justify-between">
           <div className="text-sm font-medium">Injury Risk</div>
           <div className="text-sm font-medium">{choice.injuredRisk}%</div>
