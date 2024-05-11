6acdd328d2e0a7cb8015c32dafafc00786cb0e42
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-05-11 16:50:48 +0200

    feat: add update view #60

--- src/modules/tournament/model/TournamentModel.ts
+++ src/modules/tournament/model/TournamentModel.ts
@@ -17,6 +17,7 @@ export class TournamentModel {
   private _pools: Character[][] = [];
   private _finalRankings: Character[] = [];
   private _isInPool: boolean = false;
+  private _state: string = 'init';
 
   constructor(
     tournamentManagerPresenter: TournamentManagerPresenter,
@@ -64,6 +65,10 @@ export class TournamentModel {
     return this._isTournamentStarted;
   }
 
+  get state(): string {
+    return this._state;
+  }
+
   initTournament() {
     this._season = this.tournamentManagerPresenter.gameCorePresenter.getCurrentSeason();
     const pools: Character[][] = [];
@@ -76,6 +81,11 @@ export class TournamentModel {
       pools[i % nbPools].push(characters[i]);
     }
     this._pools = pools;
+    console.log(this._isInPool);
+    this._isInPool = true;
+    this._isTournamentStarted = true;
+    this._state = 'pool';
+    console.log(this._isInPool);
   }
 
   playRoundInPool(poolNo: number) {
