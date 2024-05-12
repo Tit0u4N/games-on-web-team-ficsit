b00d4f102fe3fe06c5222753f920618183d80c11
Titouan Lacombe--Fabre <titouan.lacombe--fabre@etu.univ-cotedazur.fr>
2024-05-12 12:47:04 +0200

    feat: Bracketv1 finish #60

---
+++ src/modules/tournament/view/React/bracket/Bracketv1Candidate.tsx
@@ -0,0 +1,36 @@
+import { Avatar, Badge } from '@nextui-org/react';
+import React from 'react';
+import { Country } from '../../../../../core/Country.ts';
+
+type Props = {
+  candidate: Candidate;
+  className?: string;
+};
+
+export type Candidate = {
+  name: string;
+  country: Country;
+  image: string;
+  score?: number;
+};
+
+export const Bracketv1Candidate: React.FC<Props> = ({ candidate }) => {
+  return (
+    <div className={'h-[45px] w-[42%] ms-[20px] flex justify-center rounded-xl bg-default'}>
+      <div className={'flex flex-col items-center justify-center size-[40px] ms-[-20px]'}>
+        <Badge color={'default'} placement={'bottom-right'} content={candidate.country.getFlag()}>
+          <Avatar
+            showFallback
+            fallback={<h1 className={'text-4xl font-semibold text-gray-600 a'}> ? </h1>}
+            src={candidate.image}
+            isBordered
+            radius={'full'}
+            size={'md'}
+            color={'default'}
+          />
+        </Badge>
+      </div>
+      <div className={'w-full flex flex-col items-center justify-center'}>{candidate.name}</div>
+    </div>
+  );
+};
