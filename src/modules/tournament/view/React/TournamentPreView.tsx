c74260ed8fe23d617edca36f0df75ab07c9d6037
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-05-23 20:56:34 +0200

    feat: add sport to preview #60

--- src/modules/tournament/view/React/TournamentPreView.tsx
+++ src/modules/tournament/view/React/TournamentPreView.tsx
@@ -10,6 +10,7 @@ export const TournamentPreView: React.FC<Props> = ({ tournament }) => {
     <div className={'m-4 h-100 border-2 p-4 rounded-lg'}>
       <p>Difficulty: {tournament.tournamentModel.difficulty}</p>
       <p>Number of bracket: {tournament.tournamentModel.numberRound}</p>
+      <p>Sport: {tournament.tournamentModel.sport.name}</p>
       <p>{tournament.tournamentModel.reward.reward?.name}</p>
       <p>{tournament.tournamentModel.reward.reward?.name}</p>
     </div>
