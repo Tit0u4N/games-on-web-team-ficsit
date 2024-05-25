91e886068546d06bd2522a77f1228980f7c88374
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-05-25 16:40:19 +0200

    feat: add arena model #76

--- src/modules/building/view/Babylon/ArenaView.ts
+++ src/modules/building/view/Babylon/ArenaView.ts
@@ -1,55 +1,34 @@
-import {
-  ActionManager,
-  Color4,
-  ExecuteCodeAction,
-  Mesh,
-  MeshBuilder,
-  Scene,
-  StandardMaterial,
-  Vector3,
-} from '@babylonjs/core';
+import { ActionManager, ExecuteCodeAction, Mesh, Scene, Vector3 } from '@babylonjs/core';
 import { ArenaModel } from '@building/model/ArenaModel.ts';
 import { ViewInitable } from '@/core/Interfaces.ts';
 import { ArenaPresenter } from '@building/presenter/ArenaPresenter.ts';
+import { importModel } from '@core/ModelImporter.ts';
 
 export class ArenaView implements ViewInitable {
   private arenaModel: ArenaModel;
   private scene!: Scene;
   private _mesh!: Mesh;
   private arenaPresenter: ArenaPresenter;
+  private static readonly SCALE = 0.5;
 
   constructor(arenaPresenter: ArenaPresenter) {
     this.arenaPresenter = arenaPresenter;
     this.arenaModel = this.arenaPresenter.arena;
   }
 
-  initView(scene: Scene) {
+  async initView(scene: Scene) {
     this.scene = scene;
-    this.createMesh(this.arenaModel.position);
+    await this.createMesh(this.arenaModel.position);
     this.addActionManager();
   }
 
   // Create a cube above the tile
-  public createMesh(vector: Vector3): void {
-    // add red color
-    const cube = MeshBuilder.CreateBox(
-      'cube_tile',
-      {
-        size: 1,
-        faceColors: [
-          new Color4(1, 0, 0, 1),
-          new Color4(1, 0, 0, 1),
-          new Color4(1, 0, 0, 1),
-          new Color4(1, 0, 0, 1),
-          new Color4(1, 0, 0, 1),
-          new Color4(1, 0, 0, 1),
-        ],
-      },
-      this.scene,
-    );
-    cube.position = vector.add(new Vector3(0, 0, 0));
-    cube.material = new StandardMaterial('material_cube', this.scene);
-    this._mesh = cube;
+  public async createMesh(vector: Vector3) {
+    const mesh = await importModel('arena.glb', { scene: this.scene, path: 'arena/', multiMaterial: true });
+    if (!mesh) throw new Error('Mesh not found');
+    mesh.scaling = new Vector3(ArenaView.SCALE, ArenaView.SCALE, ArenaView.SCALE);
+    mesh.position = vector.add(new Vector3(0, 0.1, 0));
+    this._mesh = mesh;
   }
 
   private addActionManager(): void {
