9fc370d25a6f84e1168959bbab1b493d83cd0598
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-05-26 22:46:58 +0200

    feat: add inventory in tournament #60

--- src/modules/tournament/view/React/TournamentBracketView.tsx
+++ src/modules/tournament/view/React/TournamentBracketView.tsx
@@ -2,6 +2,7 @@ import { TournamentPresenter } from '../../presenter/TournamentPresenter.ts';
 import { BracketObject, Bracketv1 } from './bracket/Bracketv1.tsx';
 import React from 'react';
 import { Button, Divider, ModalBody } from '@nextui-org/react';
+import InventoriesModal from '../../../inventory/view/React/InventoriesModal.tsx';
 
 interface Props {
   tournament: TournamentPresenter;
@@ -33,23 +34,48 @@ export const TournamentBracketView: React.FC<Props> = ({ tournament, onStatusCha
     onStatusChange();
   };
 
+  const gameCorePresenter = tournament.tournamentManagerPresenter.gameCorePresenter;
+  const [inventoryOpen, setInventoryOpen] = React.useState(false);
+
+  const toggleModal = () => {
+    setInventoryOpen(!inventoryOpen);
+  };
+
+  const isModalOpen = () => {
+    return inventoryOpen;
+  };
+
   return (
-    <ModalBody>
-      <div className={'flex gap-3 h-full m-1'}>
-        <Bracketv1 bracket={bracketObjects} />
-        <Divider orientation={'vertical'} />
-        <div className={'flex-col flex gap-2 p-4 mr-4 h-full justify-center'}>
-          <Button onPress={() => nextRound()} isDisabled={isNextRoundDisabled}>
-            Next round
-          </Button>
-          {!tournament.tournamentModel.isUserCharacterStillInTournament() && (
-            <>
-              <p>All your athletes have been eliminated</p>
-              <Button onPress={() => skipTournament()}>Skip tournament</Button>
-            </>
-          )}
+    <>
+      <InventoriesModal
+        inventories={gameCorePresenter.getInventoryList()}
+        toggleModal={toggleModal}
+        isModalOpen={isModalOpen}
+        gameCorePresenter={gameCorePresenter}
+      />
+      <ModalBody>
+        <div className={'flex gap-3 h-full m-1'}>
+          <Bracketv1 bracket={bracketObjects} />
+          <Divider orientation={'vertical'} />
+          <div className={'flex-col flex gap-2 p-4 mr-4 h-full justify-center'}>
+            <Button
+              onPress={() => {
+                setInventoryOpen(true);
+              }}>
+              Inventory
+            </Button>
+            <Button onPress={() => nextRound()} isDisabled={isNextRoundDisabled} className="my-3">
+              Next round
+            </Button>
+            {!tournament.tournamentModel.isUserCharacterStillInTournament() && (
+              <>
+                <p>All your athletes have been eliminated</p>
+                <Button onPress={() => skipTournament()}>Skip tournament</Button>
+              </>
+            )}
+          </div>
         </div>
-      </div>
-    </ModalBody>
+      </ModalBody>
+    </>
   );
 };
