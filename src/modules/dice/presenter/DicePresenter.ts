ba35a25957a333a7d056e9cafe824418b1e237be
Titouan Lacombe--Fabre <titouan.lacombe--fabre@etu.univ-cotedazur.fr>
2024-05-30 10:12:01 +0200

    fix: test #97

--- src/modules/dice/presenter/DicePresenter.ts
+++ src/modules/dice/presenter/DicePresenter.ts
@@ -26,6 +26,8 @@ export class DicePresenter implements Reactable, ViewInitable {
 
   private _handleDiceValue!: (value: number) => void;
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-expect-error
   private state: 'idle' | 'rolling' | 'rolled' = 'idle';
 
   constructor(scene: Scene, diceHandler: DiceHandler) {
