0060ae59c14d6ac88601299c28c7905e0e5fb83e
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-05-30 20:32:19 +0200

    feat: add inventory #100

--- src/modules/gamecore/view/React/OlympicsView.tsx
+++ src/modules/gamecore/view/React/OlympicsView.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Button, Tab, Tabs } from '@nextui-org/react';
 import { PersonalStanding } from '@/modules/olympics/view/React/PersonalStanding.tsx';
 import { TeamStanding } from '@/modules/olympics/view/React/TeamStanding.tsx';
+import InventoriesModal from '@inventory/view/React/InventoriesModal.tsx';
 
 interface OlympicsViewProps {
   presenter: GameCorePresenter;
@@ -10,8 +11,24 @@ interface OlympicsViewProps {
 
 export const OlympicsView: React.FC<OlympicsViewProps> = ({ presenter }) => {
   const olympicsPresenter = presenter.olympicsPresenter;
+  const [inventoryOpen, setInventoryOpen] = React.useState(false);
+
+  const toggleModal = () => {
+    setInventoryOpen(!inventoryOpen);
+  };
+
+  const isModalOpen = () => {
+    return inventoryOpen;
+  };
+
   return (
     <div className={'h-[100vh] w-[100vw] bg-menu bg-cover'}>
+      <InventoriesModal
+        inventories={presenter.getInventoryList()}
+        toggleModal={toggleModal}
+        isModalOpen={isModalOpen}
+        gameCorePresenter={presenter}
+      />
       <div className={'size-full backdrop-blur flex items-center justify-center '}>
         <div className={'w-[85%] h-[85%]'}>
           <Tabs
@@ -36,7 +53,7 @@ export const OlympicsView: React.FC<OlympicsViewProps> = ({ presenter }) => {
           </Tabs>
           <div className="absolute bottom-[10%] h-[25%] flex flex-col-reverse gap-3">
             <Button>Next tournaments</Button>
-            <Button>Inventory</Button>
+            <Button onPress={() => setInventoryOpen(true)}>Inventory</Button>
           </div>
         </div>
       </div>
