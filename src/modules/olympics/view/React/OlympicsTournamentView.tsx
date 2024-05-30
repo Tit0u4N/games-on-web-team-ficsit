fecd907117034d92142d065732c9e812f601665b
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-05-30 22:41:23 +0200

    feat: add OlympicsTournamentView #100

--- src/modules/olympics/view/React/OlympicsTournamentView.tsx
+++ src/modules/olympics/view/React/OlympicsTournamentView.tsx
@@ -1,10 +1,29 @@
 import { OlympicsPresenter } from '@/modules/olympics/presenter/OlympicsPresenter.ts';
-import { FC } from 'react';
+import React, { FC } from 'react';
+import { TournamentBracketView } from '@tournament/view/React/TournamentBracketView.tsx';
+import { TournamentView } from '@tournament/view/React/TournamentView.tsx';
+import { TournamentEndView } from '@tournament/view/React/TournamentEndView.tsx';
 
 interface OlympicsTournamentViewProps {
   presenter: OlympicsPresenter;
+  setHideModal: (hideModal: boolean) => void;
 }
 
-export const OlympicsTournamentView: FC<OlympicsTournamentViewProps> = ({ presenter }) => {
-  return <></>;
+export const OlympicsTournamentView: FC<OlympicsTournamentViewProps> = ({ presenter, setHideModal }) => {
+  const tournamentPresenter = presenter.getCurrentTournamentPresenter();
+  const [onStatusChange, setOnStatusChange] = React.useState(true);
+  return (
+    <>
+      {tournamentPresenter.tournamentModel.tournamentStatus === 'inProgress' ? (
+        <TournamentBracketView
+          tournament={tournamentPresenter}
+          onStatusChange={() => setOnStatusChange(!onStatusChange)}
+        />
+      ) : tournamentPresenter.tournamentModel.tournamentStatus === 'inPool' ? (
+        <TournamentView tournament={tournamentPresenter} setHideModal={setHideModal} />
+      ) : (
+        <TournamentEndView tournament={tournamentPresenter} />
+      )}
+    </>
+  );
 };
