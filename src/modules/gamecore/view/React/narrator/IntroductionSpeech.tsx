e4bf543fdfa53c61eeba7d080afb093dc80ee78b
palotas tamas <tamas.palotas@etu.univ-cotedazur.fr>
2024-06-01 00:03:42 +0200

    fix: svg bar choices cards path #00

--- src/modules/gamecore/view/React/narrator/IntroductionSpeech.tsx
+++ src/modules/gamecore/view/React/narrator/IntroductionSpeech.tsx
@@ -1,54 +1,54 @@
-import {Button} from "@nextui-org/react";
-import React, {useEffect, useState} from "react";
-import {config} from "@core/Interfaces.ts";
+import { Button } from '@nextui-org/react';
+import React, { useEffect, useState } from 'react';
+import { config } from '@core/Interfaces.ts';
 
 interface IntroductionSpeechProps {
-    speeches: string[];
-    onComplete: () => void;
+  speeches: string[];
+  onComplete: () => void;
 }
 
-export const IntroductionSpeech: React.FC<IntroductionSpeechProps> = ({speeches, onComplete}) => {
-    const [currentSpeechIndex, setCurrentSpeechIndex] = useState(0);
-    const [displayedText, setDisplayedText] = useState('');
-    const [isComplete, setIsComplete] = useState(false);
+export const IntroductionSpeech: React.FC<IntroductionSpeechProps> = ({ speeches, onComplete }) => {
+  const [currentSpeechIndex, setCurrentSpeechIndex] = useState(0);
+  const [displayedText, setDisplayedText] = useState('');
+  const [isComplete, setIsComplete] = useState(false);
 
-    useEffect(() => {
-        if (currentSpeechIndex < speeches.length) {
-            let index = 0;
-            const interval = setInterval(() => {
-                setDisplayedText(speeches[currentSpeechIndex].substring(0, index));
-                if (index >= speeches[currentSpeechIndex].length) {
-                    clearInterval(interval);
-                    setIsComplete(true);
-                } else {
-                    index++;
-                }
-            }, config.narratorBox.speed);
-
-            return () => clearInterval(interval);
+  useEffect(() => {
+    if (currentSpeechIndex < speeches.length) {
+      let index = 0;
+      const interval = setInterval(() => {
+        setDisplayedText(speeches[currentSpeechIndex].substring(0, index));
+        if (index >= speeches[currentSpeechIndex].length) {
+          clearInterval(interval);
+          setIsComplete(true);
         } else {
-            onComplete();
+          index++;
         }
-    }, [currentSpeechIndex, speeches, onComplete]);
+      }, config.narratorBox.speed);
 
-    const handleClick = () => {
-        if (isComplete) {
-            setIsComplete(false);
-            setDisplayedText('');
-            setCurrentSpeechIndex((prevIndex) => prevIndex + 1);
-        }
-    };
+      return () => clearInterval(interval);
+    } else {
+      onComplete();
+    }
+  }, [currentSpeechIndex, speeches, onComplete]);
 
-    return (
-        <div className="bg-white rounded-lg flex-1 py-2">
-            <p className={'ml-6 px-6'}>{displayedText}</p>
-            {isComplete && (
-                <div className={'w-full flex justify-end'}>
-                    <Button className="mt-4" onClick={handleClick}>
-                        {currentSpeechIndex === speeches.length - 1 ? 'Close' : 'Next'}
-                    </Button>
-                </div>
-            )}
+  const handleClick = () => {
+    if (isComplete) {
+      setIsComplete(false);
+      setDisplayedText('');
+      setCurrentSpeechIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <div className="bg-white rounded-lg flex-1 py-2">
+      <p className={'ml-6 px-6'}>{displayedText}</p>
+      {isComplete && (
+        <div className={'w-full flex justify-end'}>
+          <Button className="mt-4" onClick={handleClick}>
+            {currentSpeechIndex === speeches.length - 1 ? 'Close' : 'Next'}
+          </Button>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
